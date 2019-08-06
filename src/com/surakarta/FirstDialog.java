7163c0f9618fe6b72b796e2b6282b159236eb662
xiepl1997 <xiepl1997@gmail.com>
2019-08-06 23:59:20 +0800

    update

--- src/com/surakarta/FirstDialog.java
+++ src/com/surakarta/FirstDialog.java
@@ -98,6 +98,7 @@ public class FirstDialog extends JFrame implements ActionListener{
 				return;
 			}
 			this.dispose();
+			
 		}
 		
 	}
--- src/com/surakarta/MainDialog.java
+++ src/com/surakarta/MainDialog.java
@@ -14,8 +14,10 @@ import javax.swing.GroupLayout.Alignment;
 import javax.swing.JPanel;
 import javax.swing.JSplitPane;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.border.TitledBorder;
+import javax.xml.ws.AsyncHandler;
 import javax.swing.UIManager;
 import javax.swing.JTextField;
 import java.awt.Font;
@@ -89,9 +91,9 @@ public class MainDialog extends JFrame implements MouseListener {
 
 		playStatus = new JLabel("");
 		playStatus.setForeground(Color.ORANGE);
-		playStatus.setFont(new Font("黑体", Font.BOLD, 21));
+		playStatus.setFont(new Font("黑体", Font.BOLD, 19));
 		GroupLayout gl_panel_1 = new GroupLayout(panel_1);
-		gl_panel_1.setHorizontalGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING)
+		gl_panel_1.setHorizontalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup()
 						.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
 								.addGroup(gl_panel_1.createSequentialGroup().addContainerGap(85, Short.MAX_VALUE)
@@ -100,13 +102,12 @@ public class MainDialog extends JFrame implements MouseListener {
 										.addComponent(label, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
 										.addPreferredGap(ComponentPlacement.RELATED)))
 						.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
-								.addComponent(red_positon_text, GroupLayout.PREFERRED_SIZE, 122,
-										GroupLayout.PREFERRED_SIZE)
-								.addComponent(red_points_text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-										GroupLayout.PREFERRED_SIZE))
+								.addComponent(red_points_text, 112, 112, 112)
+								.addComponent(red_positon_text, GroupLayout.DEFAULT_SIZE, 112, Short.MAX_VALUE))
 						.addGap(82))
-				.addGroup(Alignment.LEADING, gl_panel_1.createSequentialGroup().addContainerGap()
-						.addComponent(playStatus, GroupLayout.DEFAULT_SIZE, 311, Short.MAX_VALUE).addContainerGap()));
+				.addGroup(gl_panel_1.createSequentialGroup()
+						.addComponent(playStatus, GroupLayout.PREFERRED_SIZE, 283, GroupLayout.PREFERRED_SIZE)
+						.addContainerGap(38, Short.MAX_VALUE)));
 		gl_panel_1.setVerticalGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_1.createSequentialGroup().addGap(53)
 						.addGroup(gl_panel_1.createParallelGroup(Alignment.BASELINE).addComponent(label).addComponent(
@@ -129,16 +130,12 @@ public class MainDialog extends JFrame implements MouseListener {
 		gl_panel.setHorizontalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel.createSequentialGroup().addGap(90)
 						.addComponent(label_1, GroupLayout.PREFERRED_SIZE, 32, GroupLayout.PREFERRED_SIZE)
-						.addPreferredGap(ComponentPlacement.UNRELATED).addComponent(black_points_text,
-								GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addContainerGap(133, Short.MAX_VALUE)));
+						.addPreferredGap(ComponentPlacement.UNRELATED)
+						.addComponent(black_points_text, GroupLayout.DEFAULT_SIZE, 119, Short.MAX_VALUE).addGap(80)));
 		gl_panel.setVerticalGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_panel.createSequentialGroup().addGap(37)
-						.addGroup(gl_panel.createParallelGroup(Alignment.BASELINE)
-								.addComponent(black_points_text, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE,
-										GroupLayout.PREFERRED_SIZE)
-								.addComponent(label_1))
-						.addContainerGap(232, Short.MAX_VALUE)));
+				.addGroup(gl_panel.createSequentialGroup().addGap(37).addGroup(gl_panel
+						.createParallelGroup(Alignment.BASELINE).addComponent(black_points_text).addComponent(label_1))
+						.addGap(232)));
 		panel.setLayout(gl_panel);
 		getContentPane().setLayout(groupLayout);
 		;
@@ -151,61 +148,58 @@ public class MainDialog extends JFrame implements MouseListener {
 
 		// 判断是否是电脑先行
 		if (this.WhoFirst == 0) {
-			// AI();
+			AI();
 		}
-
 	}
 
 	Chess chess;// 所选的棋子
 	Step step;// 所选的可落子位
-	int turn = 0; // 标记当前是选子还是落子，0为选子，1为落子
+	// int turn = 0; // 标记当前是选子还是落子，0为选子，1为落子
 	// 鼠标点击监听
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
 
-		if (turn == 0) {
-			int mins = Integer.MAX_VALUE;
-			for (int i = 0; i < myPanel.chessList.size(); i++) {// 检测点击的是否是棋子
-				chess = myPanel.chessList.get(i);
-				if (chess.color == Color.red) {
-					float x = Math.abs(e.getX() - chess.position.x);
-					float y = Math.abs(e.getY() - chess.position.y);
-					float z = (float) Math.sqrt(x * x + y * y);// 计算鼠标点击到每个chess的距离
-					if (z < 42) {
-						red_positon_text.setText("第" + chess.x + "行,第" + chess.y + "列");
-						playStatus.setText("您已选择棋子，请决定下一步。");
-						setStep();
-						myPanel.repaint();
-						turn = 1;
-						return;
-					}
-				}
-			}
-		} 
-		else {
-			int mins = Integer.MAX_VALUE;
-			for (int i = 0; i < myPanel.stepList.size(); i++) {// 检测点击的是否是可落子位
-				step = myPanel.stepList.get(i);
-				float x = Math.abs(e.getX() - step.position.x);
-				float y = Math.abs(e.getY() - step.position.y);
-				float z = (float) Math.sqrt(x * x + y * y);// 计算鼠标点击到每个step的距离
-				if (z < 21) {
-					myPanel.state[step.x - 1][step.y - 1] = 1;
-					myPanel.state[chess.x - 1][chess.y - 1] = 0;
-					myPanel.stepList.clear();
-					for (int j = 0; j < 6; j++) {
-						for (int k = 0; k < 6; k++)
-							myPanel.step[j][k] = 0;
-					}
-					//AI();
+		int mins = Integer.MAX_VALUE;
+		for (int i = 0; i < myPanel.chessList.size(); i++) {// 检测点击的是否是棋子
+			Chess chess1 = myPanel.chessList.get(i);
+			if (chess1.color == Color.red) {
+				float x = Math.abs(e.getX() - chess1.position.x);
+				float y = Math.abs(e.getY() - chess1.position.y);
+				float z = (float) Math.sqrt(x * x + y * y);// 计算鼠标点击到每个chess的距离
+				if (z < 42) {
+					red_positon_text.setText("第" + chess1.x + "行,第" + chess1.y + "列");
+					playStatus.setText("您已选择棋子，请决定下一步。");
+					chess = chess1;
+					setStep();
 					myPanel.repaint();
-					playStatus.setText("您已落子，请再选择棋子。");
-					turn = 0;
 					return;
 				}
 			}
 		}
+
+		mins = Integer.MAX_VALUE;
+		for (int i = 0; i < myPanel.stepList.size(); i++) {// 检测点击的是否是可落子位
+			step = myPanel.stepList.get(i);
+			float x = Math.abs(e.getX() - step.position.x);
+			float y = Math.abs(e.getY() - step.position.y);
+			float z = (float) Math.sqrt(x * x + y * y);// 计算鼠标点击到每个step的距离
+			if (z < 21) {
+				myPanel.state[step.x - 1][step.y - 1] = 1;
+				myPanel.state[chess.x - 1][chess.y - 1] = 0;
+				myPanel.stepList.clear();
+				for (int j = 0; j < 6; j++) {
+					for (int k = 0; k < 6; k++)
+						myPanel.step[j][k] = 0;
+				}
+				//AI();
+				myPanel.repaint();
+				getGrade();
+				playStatus.setText("您已落子，请再选择棋子。");
+				return;
+			}
+		}
+
 	}
 
 	@Override
@@ -234,56 +228,231 @@ public class MainDialog extends JFrame implements MouseListener {
 
 	// 将所选棋子可走的点标注出来
 	public void setStep() {
+
 		myPanel.stepList.clear();
+
+		//
 		for (int i = 0; i < 6; i++) {
 			for (int j = 0; j < 6; j++)
 				myPanel.step[i][j] = 0;
 		}
 		if (chess.x - 1 - 1 >= 0) {
-			if (myPanel.state[chess.x - 1 - 1][chess.y - 1] == 0)
+			if (myPanel.state[chess.x - 1 - 1][chess.y - 1] != 1)
 				myPanel.step[chess.x - 1 - 1][chess.y - 1] = 1;
 		}
 		if (chess.x - 1 + 1 <= 5) {
-			if (myPanel.state[chess.x - 1 + 1][chess.y - 1] == 0)
+			if (myPanel.state[chess.x - 1 + 1][chess.y - 1] != 1)
 				myPanel.step[chess.x - 1 + 1][chess.y - 1] = 1;
 		}
 		if (chess.y - 1 - 1 >= 0) {
-			if (myPanel.state[chess.x - 1][chess.y - 1 - 1] == 0)
+			if (myPanel.state[chess.x - 1][chess.y - 1 - 1] != 1)
 				myPanel.step[chess.x - 1][chess.y - 1 - 1] = 1;
 		}
 		if (chess.y - 1 + 1 <= 5) {
-			if (myPanel.state[chess.x - 1][chess.y - 1 + 1] == 0)
+			if (myPanel.state[chess.x - 1][chess.y - 1 + 1] != 1)
 				myPanel.step[chess.x - 1][chess.y - 1 + 1] = 1;
 		}
 		if (chess.x - 1 - 1 >= 0 && chess.y - 1 - 1 >= 0) {
-			if (myPanel.state[chess.x - 1 - 1][chess.y - 1 - 1] == 0)
+			if (myPanel.state[chess.x - 1 - 1][chess.y - 1 - 1] != 1)
 				myPanel.step[chess.x - 1 - 1][chess.y - 1 - 1] = 1;
 		}
 		if (chess.x - 1 + 1 <= 5 && chess.y - 1 - 1 >= 0) {
-			if (myPanel.state[chess.x - 1 + 1][chess.y - 1 - 1] == 0)
+			if (myPanel.state[chess.x - 1 + 1][chess.y - 1 - 1] != 1)
 				myPanel.step[chess.x - 1 + 1][chess.y - 1 - 1] = 1;
 		}
 		if (chess.x - 1 - 1 >= 0 && chess.y - 1 + 1 <= 5) {
-			if (myPanel.state[chess.x - 1 - 1][chess.y - 1 + 1] == 0)
+			if (myPanel.state[chess.x - 1 - 1][chess.y - 1 + 1] != 1)
 				myPanel.step[chess.x - 1 - 1][chess.y - 1 + 1] = 1;
 		}
 		if (chess.x - 1 + 1 <= 5 && chess.y - 1 + 1 <= 5) {
-			if (myPanel.state[chess.x - 1 + 1][chess.y - 1 + 1] == 0)
+			if (myPanel.state[chess.x - 1 + 1][chess.y - 1 + 1] != 1)
 				myPanel.step[chess.x - 1 + 1][chess.y - 1 + 1] = 1;
 		}
-		
-		fly();
+
+		// up = down = left = right = 0;
+		fly_dfs(chess.x - 1, chess.y - 1, 0, 1, 0);
 
 	}
-	
-	//搜寻所选棋子的吃子走位（深度优先）
-	public void fly() {
-		
+
+	// int up = 0, down = 0, left = 0, right = 0;// 上下左右经过圆轨道标记，0表示未经过圆轨，1反之
+	int flag = 0;// 经过圆轨标志，0未经过，1经过
+	// 搜寻所选棋子的飞行吃子走位（深度优先）
+	// direction为搜索路线方向，0四个方向，1上，2下，3左，4右，type为棋子类型:1玩家\2电脑
+
+	public void fly_dfs(int x, int y, int direction, int type, int flag) {
+
+		// 四个角
+		if ((x == 0 && y == 0) || (x == 0 && y == 5) || (x == 5 && y == 0) || (x == 5 && y == 5)) {
+			return;
+		}
+
+		// 如果是敌方棋子且经过圆弧
+		if (myPanel.state[x][y] == -type && flag == 1) {
+			myPanel.step[x][y] = 1;
+			return;
+
+		} else if (myPanel.state[x][y] == -type) {
+			return;
+		}
+
+		// 如果是己方棋子且起始点
+		if (myPanel.state[x][y] == type && direction != 0) {
+			return;
+		}
+
+		// 起始点
+		if (direction == 0) {
+
+			// 如果是切点的情况
+			if (x == 0 && y == 1) {
+				fly_dfs(1, 0, 4, type, 1);
+			} else if (x == 0 && y == 2) {
+				fly_dfs(2, 0, 4, type, 1);
+			} else if (x == 0 && y == 3) {
+				fly_dfs(2, 5, 3, type, 1);
+			} else if (x == 0 && y == 4) {
+				fly_dfs(1, 5, 3, type, 1);
+			} else if (x == 1 && y == 0) {
+				fly_dfs(0, 1, 2, type, 1);
+			} else if (x == 2 && y == 0) {
+				fly_dfs(0, 2, 2, type, 1);
+			} else if (x == 3 && y == 0) {
+				fly_dfs(5, 2, 1, type, 1);
+			} else if (x == 4 && y == 0) {
+				fly_dfs(5, 1, 1, type, 1);
+			} else if (x == 1 && y == 5) {
+				fly_dfs(0, 4, 2, type, 1);
+			} else if (x == 2 && y == 5) {
+				fly_dfs(0, 3, 2, type, 1);
+			} else if (x == 3 && y == 5) {
+				fly_dfs(5, 3, 1, type, 1);
+			} else if (x == 4 && y == 5) {
+				fly_dfs(5, 4, 1, type, 1);
+			} else if (x == 5 && y == 1) {
+				fly_dfs(4, 0, 4, type, 1);
+			} else if (x == 5 && y == 2) {
+				fly_dfs(3, 0, 4, type, 1);
+			} else if (x == 5 && y == 3) {
+				fly_dfs(3, 5, 3, type, 1);
+			} else if (x == 5 && y == 4) {
+				fly_dfs(4, 5, 3, type, 1);
+			}
+
+			// 上
+			if (x - 1 >= 0) {
+				fly_dfs(x - 1, y, 1, type, flag);
+			}
+
+			// 下
+			if (x + 1 <= 5) {
+				fly_dfs(x + 1, y, 2, type, flag);
+			}
+
+			// 左
+			if (y - 1 >= 0) {
+				fly_dfs(x, y - 1, 3, type, flag);
+			}
+
+			// 右
+			if (y + 1 <= 5) {
+				fly_dfs(x, y + 1, 4, type, flag);
+			}
+		} else {
+			
+			//如果是切点
+			if (x == 0 && y == 1 && direction == 1) {
+				fly_dfs(1, 0, 4, type, 1);
+			} else if (x == 0 && y == 2 && direction == 1) {
+				fly_dfs(2, 0, 4, type, 1);
+			} else if (x == 0 && y == 3 && direction == 1) {
+				fly_dfs(2, 5, 3, type, 1);
+			} else if (x == 0 && y == 4 && direction == 1) {
+				fly_dfs(1, 5, 3, type, 1);
+			} else if (x == 1 && y == 0 && direction == 3) {
+				fly_dfs(0, 1, 2, type, 1);
+			} else if (x == 2 && y == 0 && direction == 3) {
+				fly_dfs(0, 2, 2, type, 1);
+			} else if (x == 3 && y == 0 && direction == 3) {
+				fly_dfs(5, 2, 1, type, 1);
+			} else if (x == 4 && y == 0 && direction == 3) {
+				fly_dfs(5, 1, 1, type, 1);
+			} else if (x == 1 && y == 5 && direction == 4) {
+				fly_dfs(0, 4, 2, type, 1);
+			} else if (x == 2 && y == 5 && direction == 4) {
+				fly_dfs(0, 3, 2, type, 1);
+			} else if (x == 3 && y == 5 && direction == 4) {
+				fly_dfs(5, 3, 1, type, 1);
+			} else if (x == 4 && y == 5 && direction == 4) {
+				fly_dfs(5, 4, 1, type, 1);
+			} else if (x == 5 && y == 1 && direction == 2) {
+				fly_dfs(4, 0, 4, type, 1);
+			} else if (x == 5 && y == 2 && direction == 2) {
+				fly_dfs(3, 0, 4, type, 1);
+			} else if (x == 5 && y == 3 && direction == 2) {
+				fly_dfs(3, 5, 3, type, 1);
+			} else if (x == 5 && y == 4 && direction == 2) {
+				fly_dfs(4, 5, 3, type, 1);
+			}
+			
+			// 上
+			if (direction == 1) {
+				if (x - 1 >= 0) {
+					fly_dfs(x - 1, y, direction, type, flag);
+				}
+			}
+
+			// 下
+			if (direction == 2) {
+				if (x + 1 <= 5) {
+					fly_dfs(x + 1, y, direction, type, flag);
+				}
+			}
+
+			// 左
+			if (direction == 3) {
+				if (y - 1 >= 0) {
+					fly_dfs(x, y - 1, direction, type, flag);
+				}
+			}
+
+			// 右
+			if (direction == 4) {
+				if (y + 1 <= 5) {
+					fly_dfs(x, y + 1, direction, type, flag);
+				}
+			}
+		}
+	}
+
+	// 机器AI
+	public void AI() {
+		myPanel.state[1][0] = 0;
+		myPanel.state[2][0] = -1;
+	}
+
+	// 检测当前分数
+	public void getGrade() {
+		int computer_grade = 12;
+		int human_grade = 12;
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++)
+				if (myPanel.state[i][j] == -1) {
+					human_grade -= 1;
+				} else if (myPanel.state[i][j] == 1) {
+					computer_grade -= 1;
+				}
+		}
+		black_points_text.setText(computer_grade + "");
+		red_points_text.setText(human_grade + "");
 	}
 	
-	//对战ai
-	public void AI() {
-		
+	public void CheckWinner() {
+		if(black_points_text.getText().equals("12")) {
+			int res = JOptionPane.showConfirmDialog(null, "电脑获胜！很遗憾，你落败了！是否再来一局？", "提示", JOptionPane.YES_NO_OPTION);
+			if(res == JOptionPane.YES_OPTION) {
+				FirstDialog firstDialog = new Fir
+			}
+		}
 	}
 
 }
