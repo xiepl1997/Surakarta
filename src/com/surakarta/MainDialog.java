255ee9a4e062585a15f8449182777b41f244e9fb
xiepl1997 <xiepl1997@gmail.com>
2019-08-09 21:13:30 +0800

    添加AI

--- src/com/surakarta/MainDialog.java
+++ src/com/surakarta/MainDialog.java
@@ -2,11 +2,13 @@ package com.surakarta;
 
 import javax.swing.JFrame;
 
+import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.ArrayList;
+import java.util.function.IntPredicate;
 
 import javax.annotation.processing.RoundEnvironment;
 import javax.swing.GroupLayout;
@@ -21,6 +23,8 @@ import javax.xml.ws.AsyncHandler;
 import javax.swing.UIManager;
 import javax.swing.JTextField;
 import java.awt.Font;
+import java.awt.Point;
+import java.awt.Robot;
 
 public class MainDialog extends JFrame implements MouseListener {
 
@@ -35,8 +39,11 @@ public class MainDialog extends JFrame implements MouseListener {
 			{ 0, -1 } };
 
 	// 棋盘棋子位置，-1位黑子，1为红子
-	public int[][] board = { { -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1 }, { 0, 0, 0, 0, 0, 0 },
+	public int[][] state_temp = { { -1, -1, -1, -1, -1, -1 }, { -1, -1, -1, -1, -1, -1 }, { 0, 0, 0, 0, 0, 0 },
 			{ 0, 0, 0, 0, 0, 0 }, { 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 1, 1, 1 } };
+	public int[][] step_temp = { { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 },
+			{ 0, 0, 0, 0, 0, 0 }, { 0, 0, 0, 0, 0, 0 } };
+
 	private JTextField red_positon_text;
 	private JTextField red_points_text;
 	private JTextField black_points_text;
@@ -148,7 +155,8 @@ public class MainDialog extends JFrame implements MouseListener {
 
 		// 判断是否是电脑先行
 		if (this.WhoFirst == 0) {
-			AI();
+			myPanel.state[1][0] = 0;
+			myPanel.state[2][0] = -1;
 		}
 	}
 
@@ -187,16 +195,24 @@ public class MainDialog extends JFrame implements MouseListener {
 			if (z < 21) {
 				myPanel.state[step.x - 1][step.y - 1] = 1;
 				myPanel.state[chess.x - 1][chess.y - 1] = 0;
+				for (int j = 0; j < 6; j++) {
+					for (int k = 0; k < 6; k++)
+						myPanel.step[j][k] = 0;
+				}
+				
 				myPanel.stepList.clear();
 				for (int j = 0; j < 6; j++) {
 					for (int k = 0; k < 6; k++)
 						myPanel.step[j][k] = 0;
 				}
-				//AI();
+
+				playStatus.setText("您已落子，请再选择棋子。");
+
+				AI();
+
 				myPanel.repaint();
 				getGrade();
-				playStatus.setText("您已落子，请再选择棋子。");
-				return;
+				CheckWinner();
 			}
 		}
 
@@ -270,8 +286,23 @@ public class MainDialog extends JFrame implements MouseListener {
 		}
 
 		// up = down = left = right = 0;
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j < 6; j++){
+				state_temp[i][j] = myPanel.state[i][j];
+			}
+		}
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j < 6; j++){
+				step_temp[i][j] = myPanel.step[i][j];
+			}
+		}
 		fly_dfs(chess.x - 1, chess.y - 1, 0, 1, 0);
-
+		//myPanel.state = state_temp.clone();
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j < 6; j++){
+				myPanel.step[i][j] = step_temp[i][j];
+			}
+		}
 	}
 
 	// int up = 0, down = 0, left = 0, right = 0;// 上下左右经过圆轨道标记，0表示未经过圆轨，1反之
@@ -287,16 +318,16 @@ public class MainDialog extends JFrame implements MouseListener {
 		}
 
 		// 如果是敌方棋子且经过圆弧
-		if (myPanel.state[x][y] == -type && flag == 1) {
-			myPanel.step[x][y] = 1;
+		if (state_temp[x][y] == -type && flag == 1) {
+			step_temp[x][y] = 1;
 			return;
 
-		} else if (myPanel.state[x][y] == -type) {
+		} else if (state_temp[x][y] == -type) {
 			return;
 		}
 
-		// 如果是己方棋子且起始点
-		if (myPanel.state[x][y] == type && direction != 0) {
+		// 如果是己方棋子
+		if (state_temp[x][y] == type && direction != 0) {
 			return;
 		}
 
@@ -358,8 +389,8 @@ public class MainDialog extends JFrame implements MouseListener {
 				fly_dfs(x, y + 1, 4, type, flag);
 			}
 		} else {
-			
-			//如果是切点
+
+			// 如果是切点
 			if (x == 0 && y == 1 && direction == 1) {
 				fly_dfs(1, 0, 4, type, 1);
 			} else if (x == 0 && y == 2 && direction == 1) {
@@ -393,7 +424,7 @@ public class MainDialog extends JFrame implements MouseListener {
 			} else if (x == 5 && y == 4 && direction == 2) {
 				fly_dfs(4, 5, 3, type, 1);
 			}
-			
+
 			// 上
 			if (direction == 1) {
 				if (x - 1 >= 0) {
@@ -424,10 +455,262 @@ public class MainDialog extends JFrame implements MouseListener {
 		}
 	}
 
+	ArrayList<Point> startPoints = new ArrayList<Point>(); // 用于存储每个遍历的点
+	ArrayList<Point> points_list = new ArrayList<Point>(); // 用来存储最优选择点
+	ArrayList<Integer> score_list = new ArrayList<Integer>(); // 用来存储每个电脑棋子的最优选择点的预估分
+
+	
 	// 机器AI
 	public void AI() {
-		myPanel.state[1][0] = 0;
-		myPanel.state[2][0] = -1;
+
+		startPoints.clear();
+		points_list.clear();
+		score_list.clear();
+		
+		int x = 0;
+		int y = 0;
+
+		int maxs = Integer.MIN_VALUE;
+		int f = 0;
+		int score = 0;
+
+		// 遍历所有的电脑棋子
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				
+				
+				if (myPanel.state[i][j] == -1) {
+					for(int m = 0; m < 6; m++) {
+						for(int n = 0; n < 6; n++) {
+							state_temp[m][n] = myPanel.state[m][n];
+						}
+					}
+					
+					for (int m = 0; m < 6; m++) {
+						for (int n = 0; n < 6; n++)
+							step_temp[m][n] = 0;
+					}
+					
+					search1(i, j);
+				}
+			}
+		}
+		
+		for(int i = 0; i < score_list.size(); i++) {
+			System.out.println(score_list.get(i));
+			System.out.println(startPoints.get(i).x+" "+startPoints.get(i).y);
+			System.out.println(points_list.get(i).x+" "+points_list.get(i).y);
+			if(score_list.get(i) > maxs) {
+				maxs = score_list.get(i);
+				f = i;
+			}
+		}
+		
+		myPanel.state[points_list.get(f).x][points_list.get(f).y] = -1;
+		myPanel.state[startPoints.get(f).x][startPoints.get(f).y] = 0;
+		
+//		System.out.println(points_list.get(f).x + " " + points_list.get(f).y);
+//		System.out.println(startPoints.get(f).x + " " + startPoints.get(f).y);
+//		for(int i = 0; i < 6; i++) {
+//			for(int j = 0; j < 6; j++) {
+//				System.out.print(myPanel.state[i][j]+" ");
+//			}
+//			System.out.print("\n");
+//		}
+	}
+
+	// 电脑玩家对弈走法模拟，返回（X，Y）棋子的最佳着棋
+	public void search1(int x, int y) {
+
+		int maxs = Integer.MIN_VALUE;
+		int score = 0;
+		int xx = 0, yy = 0;
+		// 回合模拟
+		setStep1(x, y, -1);
+		
+		
+		int[][] s1 = new int[6][6];
+		int[][] s2 = new int[6][6];
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j < 6; j++){
+				s1[i][j] = state_temp[i][j];
+			}
+		}
+		for(int i = 0; i < 6; i++) {
+			for(int j = 0; j < 6; j++){
+				s2[i][j] = step_temp[i][j];
+			}
+		}
+//		
+//		for(int i = 0;i < 6; i++) {
+//			for(int j = 0; j < 6; j++) {
+//				System.out.print(step_temp[i][j]+" ");
+//			}
+//			System.out.print("\n");
+//		}
+
+		//遍历可落子点
+		for (int i = 0; i < 6; i++) {
+			for (int j = 0; j < 6; j++) {
+				
+				for(int a1 = 0; a1 < 6; a1++) {
+					for(int a2 = 0; a2 < 6; a2++){
+						state_temp[a1][a2] = s1[a1][a2];
+					}
+				}
+				for(int a1 = 0; a1 < 6; a1++) {
+					for(int a2 = 0; a2 < 6; a2++){
+						step_temp[a1][a2] = s2[a1][a2];
+					}
+				}
+				
+				if(step_temp[i][j] == 1) {
+					
+					score = 0;
+					if(state_temp[i][j] == 1) { //吃子
+						
+						score += 2;
+						state_temp[i][j] = -1;
+						state_temp[x][y] = 0;
+					}
+					else { //不吃子
+						score ++;
+						state_temp[i][j] = -1;
+						state_temp[x][y] = 0;
+					}
+					
+					//int[][] a = state_temp.clone();
+					int[][] a = new int[6][6];
+					for(int a1 = 0; a1 < 6; a1++) {
+						for(int a2 = 0; a2 < 6; a2++){
+							a[a1][a2] = state_temp[a1][a2];
+						}
+					}
+					
+					//模拟玩家
+					for(int m = 0; m < 6; m++) {
+						for(int n = 0; n < 6; n++) {
+							
+							//state_temp = a.clone();
+							for(int a1 = 0; a1 < 6; a1++) {
+								for(int a2 = 0; a2 < 6; a2++){
+									state_temp[a1][a2] = a[a1][a2];
+								}
+							}
+							
+							if(state_temp[m][n] == 1) {
+								
+								for(int x1 = 0; x1 < 6; x1++) {
+									for(int y1 = 0; y1 < 6; y1++) {
+										step_temp[x1][y1] = 0;
+									}
+								}
+								
+								int score1 = score;
+								setStep1(m, n, 1);
+								
+//								int[][] s3 = state_temp.clone();
+//								int[][] s4 = step_temp.clone();
+								int[][] s3 = new int[6][6];
+								int[][] s4 = new int[6][6];
+								for(int a1 = 0; a1 < 6; a1++) {
+									for(int a2 = 0; a2 < 6; a2++){
+										s3[a1][a2] = state_temp[a1][a2];
+									}
+								}
+								for(int a1 = 0; a1 < 6; a1++) {
+									for(int a2 = 0; a2 < 6; a2++){
+										s4[a1][a2] = step_temp[a1][a2];
+									}
+								}
+								
+								for(int o = 0; o < 6; o++) {
+									for(int p = 0; p < 6; p++) {
+										
+//										state_temp = s3.clone();
+//										step_temp = s4.clone();
+										for(int a1 = 0; a1 < 6; a1++) {
+											for(int a2 = 0; a2 < 6; a2++){
+												state_temp[a1][a2] = s3[a1][a2];
+											}
+										}
+										for(int a1 = 0; a1 < 6; a1++) {
+											for(int a2 = 0; a2 < 6; a2++){
+												step_temp[a1][a2] = s4[a1][a2];
+											}
+										}
+										
+										if(step_temp[o][p] == 1) {
+											//吃子
+											if(state_temp[o][p] == -1) {
+												score1 -= 2;
+											}
+											else {
+												score1 ++;
+											}
+										}
+									}
+								}
+								if(score1 > score1) {
+									score = score1;
+								}
+							}
+							
+						}
+					}
+					if(score > maxs) {
+						xx = i;
+						yy = j;
+						maxs = score;
+					}
+				}
+			}
+		}
+		
+		startPoints.add(new Point(x, y));
+		points_list.add(new Point(xx, yy));
+		score_list.add(maxs);
+		System.out.println(maxs);
+	}
+
+	// ai 将某个棋子可走的点标注出来
+	public void setStep1(int x, int y, int type) {
+
+		if (x - 1 >= 0) {
+			if (state_temp[x - 1][y] != type)
+				step_temp[x - 1][y] = 1;
+		}
+		if (x + 1 <= 5) {
+			if (state_temp[x + 1][y] != type)
+				step_temp[x + 1][y] = 1;
+		}
+		if (y - 1 >= 0) {
+			if (state_temp[x][y - 1] != type)
+				step_temp[x][y - 1] = 1;
+		}
+		if (y + 1 <= 5) {
+			if (state_temp[x][y + 1] != type)
+				step_temp[x][y + 1] = 1;
+		}
+		if (x - 1 >= 0 && y - 1 >= 0) {
+			if (state_temp[x - 1][y - 1] != type)
+				step_temp[x - 1][y - 1] = 1;
+		}
+		if (x + 1 <= 5 && y - 1 >= 0) {
+			if (state_temp[x + 1][y - 1] != type)
+				step_temp[x + 1][y - 1] = 1;
+		}
+		if (x - 1 >= 0 && y + 1 <= 5) {
+			if (state_temp[x - 1][y + 1] != type)
+				step_temp[x - 1][y + 1] = 1;
+		}
+		if (x + 1 <= 5 && y + 1 <= 5) {
+			if (state_temp[x + 1][y + 1] != type)
+				step_temp[x + 1][y + 1] = 1;
+		}
+
+		fly_dfs(x, y, 0, type, 0);
+
 	}
 
 	// 检测当前分数
@@ -445,12 +728,27 @@ public class MainDialog extends JFrame implements MouseListener {
 		black_points_text.setText(computer_grade + "");
 		red_points_text.setText(human_grade + "");
 	}
-	
+
+	// 获胜检查
 	public void CheckWinner() {
-		if(black_points_text.getText().equals("12")) {
+		if (black_points_text.getText().equals("12")) {
 			int res = JOptionPane.showConfirmDialog(null, "电脑获胜！很遗憾，你落败了！是否再来一局？", "提示", JOptionPane.YES_NO_OPTION);
-			if(res == JOptionPane.YES_OPTION) {
-				FirstDialog firstDialog = new Fir
+			if (res == JOptionPane.YES_OPTION) {
+				FirstDialog firstDialog = new FirstDialog();
+				firstDialog.setVisible(true);
+				this.dispose();
+			} else {
+				return;
+			}
+		}
+		if (red_points_text.getText().equals("12")) {
+			int res = JOptionPane.showConfirmDialog(null, "你获胜啦！是否再来一局？", "提示", JOptionPane.YES_NO_OPTION);
+			if (res == JOptionPane.YES_OPTION) {
+				FirstDialog firstDialog = new FirstDialog();
+				firstDialog.setVisible(true);
+				this.dispose();
+			} else {
+				return;
 			}
 		}
 	}
